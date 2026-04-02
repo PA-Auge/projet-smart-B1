c73650f1bac75b1f91c4f592ff4246ecaff74f7f
p1345lebg <p1345lebg@gmail.com>
2026-04-02 10:40:37 +0200

    test ultrason

--- src/test_ultrasonic.cpp
+++ src/test_ultrasonic.cpp
@@ -2,5 +2,20 @@
 
 #define SONAR_MAX_DISTANCE 100
 
-NewPing sonar_l = NewPing(2,3, SONAR_MAX_DISTANCE);
-NewPing sonar_l = NewPing(4,5, SONAR_MAX_DISTANCE);
+NewPing sonar_l = NewPing(9,10, SONAR_MAX_DISTANCE);
+NewPing sonar_r = NewPing(11,12, SONAR_MAX_DISTANCE);
+
+void setup() 
+{
+    Serial.begin(115200);
+}
+
+void loop() {
+    Serial.print("left :  ");
+    Serial.println(sonar_l.ping_cm());
+
+    Serial.print("right : ");
+    Serial.println(sonar_r.ping_cm());
+
+    Serial.println("----------");
+}
